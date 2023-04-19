2c139b6054c36a1393f2c823f72813f4fab2c30e
Franco Canzani <francocanzani@gmail.com>
2023-04-19 19:09:07 +0200

    Added product component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,22 +1,19 @@
 import { useState } from 'react';
 
 export default function ProductCard({ product }) {
-  const [isExpanded, setIsExpanded] = useState(true);
-
   return (
-    <div className='m-4 border-2 border-stone-100 p-12 shadow-amber-100'>
-      <img src={product.image} alt='Product picture' className='w-36' />
+    <div className='m-4 max-w-sm border-2 border-stone-100 p-8 shadow-sm shadow-amber-100'>
+      <div className='flex items-center justify-center'>
+        <img src={product.image} alt='Product picture' className='mb-4 w-36' />
+      </div>
       <h1 className='text-lg font-medium uppercase'>{product.title}</h1>
-      {isExpanded ? (
-        <>
-          <p className='italic'>{product.description}</p>
-          <button>Show more</button>
-        </>
-      ) : (
-        <p className='italic'>{product.description}.slice(0,5)</p>
-      )}
-      <span>${product.price}</span>
-      <span>{product.rate}</span>
+      <h2 className='mb-3 text-xs uppercase'>{product.category}</h2>
+      <span className='mb-6 block w-12 bg-yellow-400 text-white'>
+        {product.rating.rate}/5
+      </span>
+      <span className='inline-block rounded-sm bg-teal-700 p-1 text-xl font-semibold text-white shadow-lg shadow-slate-300'>
+        ${product.price}
+      </span>
     </div>
   );
 }
